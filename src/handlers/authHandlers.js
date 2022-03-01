2a83d0bfbc27d01324b0fac8089ae6597961c474
golibdev <ganrzullayev2000@gmail.com>
2022-03-01 22:10:47 +0500

    AAAAAAAAAAAAAAAAAAAAAAAA

--- src/handlers/authHandlers.js
+++ src/handlers/authHandlers.js
@@ -1,6 +1,7 @@
 export const isAuthenticated = () => {
-   const expireTime = localStorage.getItem('expire_time').slice(0,10).split('.');
-   const expireTimeDate = new Date(`${expireTime[2]}-${expireTime[1]}-${expireTime[0]}`);
+   const expireTime = localStorage.getItem('expire_time')
+   const sliceDate = expireTime !== null && expireTime.slice(0, 10).split('.')
+   const expireTimeDate = new Date(`${sliceDate[2]}-${sliceDate[1]}-${sliceDate[0]}`);
    const dateNow = new Date();
    const token = localStorage.getItem('token');
    if(!token && !(dateNow.getTime() > expireTimeDate.getTime())) {
